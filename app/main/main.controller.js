1364c74db34e18948c529f6ec22d371f6036fe92
Sebastian Ramirez <sramirez10r@gmail.com>
2016-05-01 10:28:36 -0400

    Zooming map by default

--- app/main/main.controller.js
+++ app/main/main.controller.js
@@ -7,5 +7,13 @@
 
   function MainController() {
     var vm = this;
+
+    angular.extend($scope, {
+      center: {
+        lat: 35.1938691,
+        lng: -82.134658,
+        zoom: 4
+      }
+    })
   }
 })();
